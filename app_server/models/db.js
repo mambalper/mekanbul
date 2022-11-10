4f7387d49a93a78d469da05999acb30ded97deaa
mambalper <aalperkaplan30@gmail.com>
2022-11-10 22:18:57 +0300

    Hafta 7

---
+++ app_server/models/db.js
@@ -0,0 +1,37 @@
+var mongoose = require("mongoose");
+require("./mekansema");
+var dbURI = "mongodb+srv://alper:ggveryeasy7@mekanbul.texs2d5.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(dbURI);
+function kapat(msg,callback){
+    mongoose.connection.close(function(){
+        console.log(msg);
+        callback();
+}
+
+);
+
+}
+process.on("SIGINT",function(){
+    kapat("Uygulama kapatıldı!",function(){
+            process.exit(0);
+    }
+    );
+}
+);
+mongoose.connection.on("connected",function(){
+console.log(dbURI+" adresindeki veritabanına bağlanıldı")
+}
+
+
+
+
+);
+mongoose.connection.on("disconnected",function(){
+    console.log(dbURI+" adresindeki veritabanı bağlantısı koptu");
+}
+
+);
+mongoose.connection.on("error",function(){
+    console.log("Bağlantı hatası");
+}
+);
