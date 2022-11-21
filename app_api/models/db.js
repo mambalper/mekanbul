474ba0fe462dd07a382b79c8920b36dfcfd8e37a
mambalper <aalperkaplan30@gmail.com>
2022-11-21 20:49:20 +0300

    Ödev 5 Commit

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose");
 require("./mekansema");
 var dbURI = "mongodb+srv://alper:ggveryeasy7@mekanbul.texs2d5.mongodb.net/?retryWrites=true&w=majority";
+//var dbURI= "mongodb://localhost/mekanbul";
 mongoose.connect(dbURI);
 function kapat(msg,callback){
     mongoose.connection.close(function(){
